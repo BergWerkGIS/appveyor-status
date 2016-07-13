354dd2133a030bb22fb158d8fa5c79178a39e57f
bergwerkgis <wb@BergWerk-GIS.at>
2016-07-13 12:23:33 +0200

    1st working version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # appveyor-status
+
+Displays status of latest AppVeyor builds.
+
+* Get AppVeyor Api Token https://ci.appveyor.com/api-token
+* Set environment variable `APPVEYOR_API_TOKEN`
+  * Windows: `SET APPVEYOR_API_TOKEN=<YOUR-API-TOKEN>`
+  * Linux: `export APPVEYOR_API_TOKEN=<YOUR-API-TOKEN>`
+* `git clone https://github.com/BergWerkGIS/appveyor-status.git`
+* `cd appveyor-status`
+* `node index`
---
+++ index.js
@@ -0,0 +1,57 @@
+var util = require('util');
+var clc = require('cli-color');
+var request = require('request');
+
+var av_base_url = 'https://ci.appveyor.com/api/';
+
+if (!process.env.APPVEYOR_API_TOKEN) {
+	console.log('no APPVEYOR_API_TOKEN');
+	process.exit(1);
+}
+
+request({
+	url: av_base_url + 'projects',
+	headers: {
+		'Authorization': 'Bearer ' + process.env.APPVEYOR_API_TOKEN,
+		'Content-type': 'application/json'
+	}
+}, function (err, response, body) {
+	if (err) {
+		console.error(clc.red(err));
+		process.exit(1);
+	}
+	var projects = JSON.parse(body);
+	projects.sort(function (a, b) { return a.name < b.name ? -1 : a.name > b.name ? 1 : 0 });
+	projects.forEach(function (project) {
+		if (!project.builds || project.builds.length < 1) {
+			console.log(util.format('[%s] no builds', project.name));
+		} else {
+			project.builds.forEach(function (build) {
+				var started = new Date(build.started);
+				var msg = util.format(
+					'[%s]\tbuild:%s %s branch:%s status:%s "%s"'
+					, project.name
+					, build.version
+					, util.format(
+						'%s.%s.%s %s:%s'
+						, started.getFullYear()
+						, ("0" + (started.getMonth() + 1)).slice(-2), ("0" + started.getDate()).slice(-2)
+						, ("0" + started.getHours()).slice(-2)
+						, ("0" + started.getMinutes()).slice(-2)
+					)
+					, build.branch
+					, build.status
+					, build.message
+				);
+				if (build.status !== 'success') {
+					console.log(clc.red(msg));
+				} else {
+					console.log(clc.green(msg));
+				}
+				// build.jobs.forEach(function (job) {
+				// 	console.log('job:', job);
+				// });
+			});
+		}
+	});
+});
